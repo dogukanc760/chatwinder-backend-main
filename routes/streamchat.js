8d1c5702a8c374064ffbda2ec37466ceeda3e3be
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2021-12-30 17:25:39 +0300

    Update streamchat.js

--- routes/streamchat.js
+++ routes/streamchat.js
@@ -134,6 +134,45 @@ https.get(options, function(res) {
 });
 })
 
+router.get('/get-by-name/:name', function(reqq, ress) {
+ 
+  const https = require('https');
+const crypto = require('crypto');
+var hostname = 'api.cloud.wowza.com'
+var path = '/api/v1.7/live_streams';
+//For security, never reveal API key in client-side code
+var wscApiKey = 'VBzDvdvO4j0x7GXnXprr3OxHDvE45pNWPLpHKltf5WUDCrBBYbV3rTtjR1oN3405';
+var wscAccessKey = 'ZV2smAgAf8Bhx5CHhDK5fjRD0xZPOzECQLulQC7l3ENrbOpCo8DsPuLhntQc3332';
+var timestamp = Math.round(new Date().getTime()/1000);
+var hmacData = (timestamp+':'+path+':'+wscApiKey);
+var signature = crypto.createHmac('sha256',wscApiKey).update(hmacData).digest('hex')
+const options = {
+hostname: hostname,
+path: path,
+headers: {
+  'wsc-access-key': wscAccessKey,
+  'wsc-timestamp': timestamp,
+  'wsc-signature': signature,
+  'Content-Type': 'application/json'
+}
+};
+https.get(options, function(res) {
+var body = '';
+res.on('data', function(data){
+  body += data;
+});
+res.on('end', function() {
+  var name = reqq.params.name;
+  console.log(JSON.parse(body));
+  var result = JSON.parse(body).filter(function(name){
+        return name.stream_name === reqq.params.name
+  });
+  ress.status(200).send(result);
+});
+}).on('error', function(e) {
+console.log(e.message);
+});
+})
 
 //start live a stream
 router.put('/start-live-stream/:id', function(reqq, ress){
