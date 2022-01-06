82bd4b9f3b01ae6dc74ef5ce1ff2e8d258977d27
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2022-01-06 19:22:29 +0300

    Update messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -21,9 +21,19 @@ router.get("/:conversationId", async (req, res) => {
     const messages = await Message.find({
       conversationId: req.params.conversationId,
     });
-    res.status(200).json(messages);
+    res.status(200).json({data: messages, message:"Success"});
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+router.delete("/:conversationId", async (req, res) => {
+  try {
+    const messages =  Message.find({conversationId:{$regex:req.params.conversationId}});
+    const result = await Message.deleteMany(messages);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
